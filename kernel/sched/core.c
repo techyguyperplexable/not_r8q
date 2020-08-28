341d9fb4f0bbd88fa508716fe6f4af5ebca70760
Qais Yousef <qais.yousef@arm.com>
2020-08-28 13:56:10 +0100

    sched/uclamp: Fix a deadlock when enabling uclamp static key
    [ Upstream commit e65855a52b479f98674998cb23b21ef5a8144b04 ]
    
    The following splat was caught when setting uclamp value of a task:
    
      BUG: sleeping function called from invalid context at ./include/linux/percpu-rwsem.h:49
    
       cpus_read_lock+0x68/0x130
       static_key_enable+0x1c/0x38
       __sched_setscheduler+0x900/0xad8
    
    Fix by ensuring we enable the key outside of the critical section in
    __sched_setscheduler()
    
    Fixes: 46609ce22703 ("sched/uclamp: Protect uclamp fast path code with static key")
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lkml.kernel.org/r/20200716110347.19553-4-qais.yousef@arm.com
    Signed-off-by: Qais Yousef <qais.yousef@arm.com>
    Signed-off-by: Sasha Levin <sashal@kernel.org>
    Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -1343,7 +1343,6 @@ static void __setscheduler_uclamp(struct task_struct *p,
 	if (likely(!(attr->sched_flags & SCHED_FLAG_UTIL_CLAMP)))
 		return;
 
-	static_branch_enable(&sched_uclamp_used);
 
 	if (attr->sched_flags & SCHED_FLAG_UTIL_CLAMP_MIN &&
 	    attr->sched_util_min != -1) {
