4dce2a1b91c9fff912b91e43b0d7ba8906547ad0
Peter Zijlstra <peterz@infradead.org>
2024-04-06 19:26:22 +0300

    sched/eevdf: Add feature comments
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -4,8 +4,18 @@
  * Using the avg_vruntime, do the right thing and preserve lag across
  * sleep+wake cycles. EEVDF placement strategy #1, #2 if disabled.
  */
+ 
 SCHED_FEAT(PLACE_LAG, true)
+
+/*
+ * Give new tasks half a slice to ease into the competition.
+ */
 SCHED_FEAT(PLACE_DEADLINE_INITIAL, true)
+
+/*
+ * Inhibit (wakeup) preemption until the current task has either matched the
+ * 0-lag point or until is has exhausted it's slice.
+ */
 SCHED_FEAT(RUN_TO_PARITY, true)
 
 /*
