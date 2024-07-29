21312869c4a0788149323c596a9efeacc6fb75db
TIMISONG-dev <dima.pilaer@gmail.com>
2024-07-30 02:02:59 +0300

    sched/fair: Adjust systemui and surfaceflinger tasks

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5679,6 +5679,11 @@ enqueue_task_fair(struct rq *rq, struct task_struct *p, int flags)
 	int task_new = !(flags & ENQUEUE_WAKEUP);
 	int idle_h_nr_running = task_has_idle_policy(p);
 
+	if(p->group_leader && (!strncmp(p->group_leader->comm, "surfaceflinger", 14) ||
+			!strncmp(p->group_leader->comm, "ndroid.systemui", 15))){
+		p->prio = 30;
+	}
+
 	/*
 	 * The code below (indirectly) updates schedutil which looks at
 	 * the cfs_rq utilization to select a frequency.
