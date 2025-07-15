1670ec3611bded6cf68b3ddabcf568e599fc09b9
Alexander Winkowski <dereference23@outlook.com>
2025-07-15 15:01:40 -0300

    sched: Apply Android tweaks manually Tunables can't be changed with CONFIG_SCHED_DEBUG=n
    https://github.com/LineageOS/android_system_core/blob/b4b3950e528cc62e874e59d052b2e9a600de708a/rootdir/init.rc#L323
    
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -76,8 +76,8 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  * (default: 6ms * (1 + ilog(ncpus)), units: nanoseconds)
  * (not default: 12ms * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_latency			= 12000000ULL;
-unsigned int normalized_sysctl_sched_latency		= 12000000ULL;
+unsigned int sysctl_sched_latency			= 10000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 10000000ULL;
 
 /*
  * Enable/disable honoring sync flag in energy-aware wakeups.
@@ -114,7 +114,7 @@ unsigned int normalized_sysctl_sched_min_granularity	= 3000000ULL;
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 5; // 8
+static unsigned int sched_nr_latency = 4;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -131,8 +131,8 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  *
  * (default: 1 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
+unsigned int sysctl_sched_wakeup_granularity		= 2000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 2000000UL;
 
 const_debug unsigned int sysctl_sched_migration_cost	= -1; //500000UL
 DEFINE_PER_CPU_READ_MOSTLY(int, sched_load_boost);
