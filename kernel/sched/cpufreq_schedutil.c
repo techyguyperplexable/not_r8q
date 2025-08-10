e2d3e8abb025a75f63acb99225e7fa388a18df11
Sultan Alsawaf <sultan@kerneltoast.com>
2025-08-10 16:43:11 -0300

    cpufreq: schedutil: Ignore rate limit when scaling up with FIE present When schedutil disregards a frequency transition due to the transition rate limit, there is no guaranteed deadline as to when the frequency transition will actually occur after the rate limit expires. For instance, depending on how long a CPU spends in a preempt/IRQs disabled context, a rate-limited frequency transition may be delayed indefinitely, until said CPU reaches the scheduler again. This also hurts tasks boosted via UCLAMP_MIN.
    For frequency transitions _down_, this only poses a theoretical loss of
    energy savings since a CPU may remain at a higher frequency than necessary
    for an indefinite period beyond the rate limit expiry.
    
    For frequency transitions _up_, however, this poses a significant hit to
    performance when a CPU is stuck at an insufficient frequency for an
    indefinitely long time. In latency-sensitive and bursty workloads
    especially, a missed frequency transition up can result in a significant
    performance loss due to a CPU operating at an insufficient frequency for
    too long.
    
    When support for the Frequency Invariant Engine (FIE) _isn't_ present, a
    rate limit is always required for the scheduler to compute CPU utilization
    with some semblance of accuracy: any frequency transition that occurs
    before the previous transition latches would result in the scheduler not
    knowing the frequency a CPU is actually operating at, thereby trashing the
    computed CPU utilization.
    
    However, when FIE support _is_ present, there's no technical requirement to
    rate limit all frequency transitions to a cpufreq driver's reported
    transition latency. With FIE, the scheduler's CPU utilization tracking is
    unaffected by any frequency transitions that occur before the previous
    frequency is latched.
    
    Therefore, ignore the frequency transition rate limit when scaling up on
    systems where FIE is present. This guarantees that transitions to a higher
    frequency cannot be indefinitely delayed, since they simply cannot be
    delayed at all.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -145,11 +145,8 @@ static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 		 * specifically wants that to happen on every update of the
 		 * policy limits.
 		 */
-		if (sg_policy->next_freq == next_freq &&
-		    !cpufreq_driver_test_flags(CPUFREQ_NEED_UPDATE_LIMITS))
-			return false;
-	} else if (sg_policy->next_freq == next_freq) {
-		return false;
+		if (cpufreq_driver_test_flags(CPUFREQ_NEED_UPDATE_LIMITS))
+			goto must_update;
 	}
 
 	if (sugov_rate_limit(sg_policy, time, next_freq)) {
@@ -157,7 +154,24 @@ static bool sugov_update_next_freq(struct sugov_policy *sg_policy, u64 time,
 		sg_policy->cached_raw_freq = sg_policy->prev_cached_raw_freq;
 		return false;
 	}
+	
+	/*
+	 * When a frequency update isn't mandatory (!need_freq_update), the rate
+	 * limit is checked again upon frequency reduction because systems with
+	 * frequency-invariant utilization bypass the rate limit check entirely
+	 * in sugov_should_update_freq(). This is done so that the rate limit
+	 * can be applied only for frequency reduction when frequency-invariant
+	 * utilization is present. Now that the next frequency is known, the
+	 * rate limit can be selectively applied to frequency reduction on such
+	 * systems. A check for arch_scale_freq_invariant() is omitted here
+	 * because unconditionally rechecking the rate limit is cheaper.
+	 */
+	if (next_freq == sg_policy->next_freq ||
+	    (next_freq < sg_policy->next_freq &&
+	     sugov_should_rate_limit(sg_policy, time)))
+		return false;
 
+must_update:
 	sg_policy->next_freq = next_freq;
 	sg_policy->last_freq_update_time = time;
 
