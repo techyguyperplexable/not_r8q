7a4e086620cc4afd6a93119c1786cd4476f58301
skye-pa1n <sonicgames772@gmail.com>
2025-08-11 18:01:21 -0300

    sched/tune: nuke cpu boosting

--- kernel/sched/tune.c
+++ kernel/sched/tune.c
@@ -705,13 +705,10 @@ boost_write(struct cgroup_subsys_state *css, struct cftype *cft,
 	    s64 boost)
 {
 	struct schedtune *st = css_st(css);
-
-	if (boost < 0 || boost > 100)
-		return -EINVAL;
-
-	st->boost = boost;
-
-	/* Update CPU boost */
+	
+	st->boost = 0;
+	
+	/* Nuke CPU boost */
 	schedtune_boostgroup_update(st->idx, st->boost);
 
 	return 0;
